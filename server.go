835f134397f51b10fb2d8be8a4300c06970f5e11
Christian Storch <ccstorch@gmai.com>
2018-03-19 22:09:07 +0000

    Origin

--- server.go
+++ server.go
@@ -16,10 +16,11 @@ type Server struct {
 	Router *mux.Router
 	DB     *sql.DB
 	Auth   *auth.Model
+	Origin []string
 }
 
 func New(db *sql.DB, jwtSignature string) Server {
-	s := Server{mux.NewRouter(), db, auth.Generate(jwtSignature)}
+	s := Server{mux.NewRouter(), db, auth.Generate(jwtSignature), []string{"*"}}
 	return s
 }
 
@@ -27,9 +28,7 @@ func (s *Server) Run(port string) {
 	log.Printf("SERVER STARTED AT PORT: %s", port)
 	methods := handlers.AllowedMethods([]string{"DELETE", "GET", "HEAD", "POST", "PUT", "OPTIONS"})
 	headers := handlers.AllowedHeaders([]string{"Origins", "Authorization", "X-Requested-With", "Content-Type", "Auth"})
-	origins := handlers.AllowedOrigins([]string{"*"})
-	// TODO: Config by env variable
-	// origins := handlers.AllowedOrigins([]string{"http://localhost:3001"})
+	origins := handlers.AllowedOrigins(s.Origin)
 
 	log.Fatal(http.ListenAndServe(port, handlers.LoggingHandler(
 		os.Stdout, handlers.CORS(origins, headers, methods)(s.Router))))
